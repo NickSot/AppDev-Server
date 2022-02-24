9469440effe059cc828dbe35faf9ec922ee16431
NickSot <nikola.s.sotirov@gmail.com>
2022-02-24 14:08:37 +0200

    update db script

--- db.sql
+++ db.sql
@@ -56,10 +56,11 @@ CREATE TABLE `users` (
   `Pass` varchar(1024) NOT NULL,
   `Avatar` blob,
   `Gender` varchar(50) DEFAULT NULL,
+  `OauthToken` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`uId`),
   UNIQUE KEY `Email` (`Email`),
   UNIQUE KEY `NickName` (`NickName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -198,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-17 15:33:16
+-- Dump completed on 2022-02-24 14:07:18
