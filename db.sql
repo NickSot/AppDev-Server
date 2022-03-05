7b2f8bd6cd1e54537888ff2deeea1f68639d1fda
NickSot <nikola.s.sotirov@gmail.com>
2022-03-05 14:45:16 +0200

    new sql script

--- db.sql
+++ db.sql
@@ -1,219 +1,84 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: localhost    Database: ourwardrobedb
--- ------------------------------------------------------
--- Server version	8.0.28
+Drop Database If Exists OurWardrobeDB;
+Create Database OurWardrobeDB;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Use OurWardrobeDB;
 
---
--- Table structure for table `clothes`
---
+-- Table creations 
 
-DROP TABLE IF EXISTS `clothes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `clothes` (
-  `cId` int NOT NULL AUTO_INCREMENT,
-  `ClothType` enum('Head','UpperBody','LowerBody','Feet','Hands','Groin') NOT NULL,
-  `Image` varchar(200) NOT NULL,
-  `OriginalWardrobeId` int NOT NULL,
-  PRIMARY KEY (`cId`),
-  KEY `OriginalWardrobeId` (`OriginalWardrobeId`),
-  CONSTRAINT `clothes_ibfk_1` FOREIGN KEY (`OriginalWardrobeId`) REFERENCES `wardrobes` (`wId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+Drop Table If Exists Users;
 
---
--- Dumping data for table `clothes`
---
+Create Table Users (
+	uId Integer Primary Key NOT NULL auto_increment,
+    Email Varchar(50) Unique NOT NULL,
+    NickName Varchar(50) Unique NOT NULL,
+    Pass Varchar(1024) NOT NULL,
+    Avatar Blob,
+    Gender Varchar(50),
+    OauthToken Varchar(100)
+);
 
-LOCK TABLES `clothes` WRITE;
-/*!40000 ALTER TABLE `clothes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `clothes` ENABLE KEYS */;
-UNLOCK TABLES;
+Drop Table If Exists Wardrobes;
 
---
--- Table structure for table `users`
---
+Create Table Wardrobes (
+	wId Integer Primary Key auto_increment NOT NULL,
+    NickName Varchar(50) NOT NULL,
+    CreationTime Timestamp,
+    WardrobeType Enum('Personal', 'Shared') NOT NULL
+);
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `uId` int NOT NULL AUTO_INCREMENT,
-  `Email` varchar(50) NOT NULL,
-  `NickName` varchar(50) NOT NULL,
-  `Pass` varchar(1024) NOT NULL,
-  `Avatar` blob,
-  `Gender` varchar(50) DEFAULT NULL,
-  `OauthToken` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`uId`),
-  UNIQUE KEY `Email` (`Email`),
-  UNIQUE KEY `NickName` (`NickName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+Drop Table If Exists Clothes;
 
---
--- Dumping data for table `users`
---
+Create Table Clothes (
+	cId Integer Primary Key auto_increment NOT NULL,
+    ClothType Enum ('Head', 'UpperBody', 'LowerBody',
+		'Feet', 'Hands', 'Groin') NOT NULL,
+	Image Varchar(200) NOT NULL,
+    OriginalWardrobeId Integer NOT NULL,
+    foreign key (OriginalWardrobeId) References Wardrobes(wId) On Delete Cascade
+);
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `BeforeUserDeleteTrigger` BEFORE DELETE ON `users` FOR EACH ROW Begin
-	Delete From UsersWardrobes Where uId = Old.uId;
-End */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+Drop Table If Exists WardrobesClothes;
 
---
--- Table structure for table `userswardrobes`
---
+Create Table WardrobesClothes (
+	Id Integer Primary Key auto_increment NOT NULL,
+    wId Integer NOT NULL,
+    cId Integer NOT NULL
+);
 
-DROP TABLE IF EXISTS `userswardrobes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `userswardrobes` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `uId` int NOT NULL,
-  `wId` int NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+Drop Table If Exists UsersWardrobes;
 
---
--- Dumping data for table `userswardrobes`
---
+Create Table UsersWardrobes (
+	Id Integer Primary Key auto_increment NOT NULL,
+    uId Integer NOT NULL,
+    wId Integer NOT NULL
+);
 
-LOCK TABLES `userswardrobes` WRITE;
-/*!40000 ALTER TABLE `userswardrobes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userswardrobes` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `BeforeUsersWardrobesDeleteTrigger` BEFORE DELETE ON `userswardrobes` FOR EACH ROW Begin
-	Delete From Wardrobes Where wId = Old.wId And (Select COUNT(*) From UsersWardrobes Where wId = Old.wId) = 0;
-End */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+-- Trigger creations 
+Drop Trigger If Exists BeforeWardrobeDeleteTrigger;
+Drop Trigger If Exists BeforeUserDeleteTrigger;
 
---
--- Table structure for table `wardrobes`
---
+Set Global triggerCheck = 1;
 
-DROP TABLE IF EXISTS `wardrobes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `wardrobes` (
-  `wId` int NOT NULL AUTO_INCREMENT,
-  `NickName` varchar(50) NOT NULL,
-  `CreationTime` timestamp NULL DEFAULT NULL,
-  `WardrobeType` enum('Personal','Shared') NOT NULL,
-  PRIMARY KEY (`wId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+Delimiter $
 
---
--- Dumping data for table `wardrobes`
---
-
-LOCK TABLES `wardrobes` WRITE;
-/*!40000 ALTER TABLE `wardrobes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wardrobes` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `BeforeWardrobeDeleteTrigger` BEFORE DELETE ON `wardrobes` FOR EACH ROW Begin
-	Delete From WardrobeUsers Where wId = Old.wId;
+Create Trigger BeforeWardrobeDeleteTrigger Before Delete On Wardrobes
+For Each Row
+Begin
     Delete From WardrobesClothes Where wId = Old.wId;
     Delete From WardrobesClothes Where cId In (Select cId From Clothes Where OriginalWardrobeId = Old.wId);
     Delete From Clothes Where OriginalWardrobeId = Old.wId;
-End */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+End$
 
---
--- Table structure for table `wardrobesclothes`
---
+Create Trigger BeforeUserDeleteTrigger Before Delete On Users
+For Each Row
+Begin
+	Delete From UsersWardrobes Where uId = Old.uId;
+End$
 
-DROP TABLE IF EXISTS `wardrobesclothes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `wardrobesclothes` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `wId` int NOT NULL,
-  `cId` int NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+Create Trigger BeforeUsersWardrobesDeleteTrigger Before Delete On UsersWardrobes
+For Each Row
+Begin
+	Delete From Wardrobes Where wId = Old.wId And (Select COUNT(*) From UsersWardrobes Where wId = Old.wId) = 0;
+End$
 
---
--- Dumping data for table `wardrobesclothes`
---
-
-LOCK TABLES `wardrobesclothes` WRITE;
-/*!40000 ALTER TABLE `wardrobesclothes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wardrobesclothes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping events for database 'ourwardrobedb'
---
-
---
--- Dumping routines for database 'ourwardrobedb'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-03-02 14:55:02
+Delimiter ;
